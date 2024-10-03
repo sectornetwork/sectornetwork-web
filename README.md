5a3ef829319d0e45e3165a7faba13a5e7efd1801
Matt Richards <mattc0m@gmail.com>
2024-10-03 20:10:22 +0000

    Updated readme. Updated schema properties for pages.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sectornetwork-web
-main website for www.sectornetwork.com
+
+to run locally
+
+```
+cd astro
+npm run dev
+```
+
+ensure you have env variables for `NOTION_TOKEN` and `NOTION_DATABASE_ID`. Reach out to get these. Thanks.
--- astro/src/content/config.js
+++ astro/src/content/config.js
@@ -1,16 +1,29 @@
 // src/content/config.ts
+import { z }  from "astro/zod";
 import { defineCollection } from "astro:content";
 import { notionLoader } from "notion-astro-loader";
+import {
+  notionPageSchema,
+  propertySchema,
+  transformedPropertySchema,
+} from "notion-astro-loader/schemas";
 
 const database = defineCollection({
   loader: notionLoader({
     auth: import.meta.env.NOTION_TOKEN,
     database_id: import.meta.env.NOTION_DATABASE_ID,
-    // Use Notion sorting and filtering
     filter: {
       property: "Hidden",
       checkbox: { equals: false },
     },
+  }),
+  schema: notionPageSchema({
+    properties: z.object({
+      // Converts to a primitive string
+      Name: transformedPropertySchema.title,
+      // Converts to a Notion API created_time object
+      Created: propertySchema.created_time.optional(),
+    }),
   }),
 });
 
